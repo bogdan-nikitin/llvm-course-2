04a10d19e50a938cbbb3eb37649f1073f29638db
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2025-03-30 15:04:14 +0300

    Extend MyArchTargetLowering with MyArchRegisterInfo

--- llvm/lib/Target/MyArch/MyArchISelLowering.cpp
+++ llvm/lib/Target/MyArch/MyArchISelLowering.cpp
@@ -40,6 +40,27 @@ MyArchTargetLowering::MyArchTargetLowering(const TargetMachine &TM,
     : TargetLowering(TM), STI(STI) {
   MYARCH_DUMP_RED
   addRegisterClass(MVT::i32, &MyArch::GPRRegClass);
+  computeRegisterProperties(STI.getRegisterInfo());
+
+  setStackPointerRegisterToSaveRestore(MyArch::R1);
+
+  // setSchedulingPreference(Sched::Source);
+
+  for (unsigned Opc = 0; Opc < ISD::BUILTIN_OP_END; ++Opc)
+    setOperationAction(Opc, MVT::i32, Expand);
+
+  setOperationAction(ISD::ADD, MVT::i32, Legal);
+  setOperationAction(ISD::MUL, MVT::i32, Legal);
+  // ...
+  setOperationAction(ISD::LOAD, MVT::i32, Legal);
+  setOperationAction(ISD::STORE, MVT::i32, Legal);
+
+  setOperationAction(ISD::Constant, MVT::i32, Legal);
+  setOperationAction(ISD::UNDEF, MVT::i32, Legal);
+
+  setOperationAction(ISD::BR_CC, MVT::i32, Custom);
+
+  setOperationAction(ISD::FRAMEADDR, MVT::i32, Legal);
 }
 
 const char *MyArchTargetLowering::getTargetNodeName(unsigned Opcode) const {
