e2ef32715dca6ed8e6086e31603d8d97935b3f85
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2025-04-09 21:45:32 +0300

    Add MyArch Builtin support

--- clang/include/clang/Basic/TargetBuiltins.h
+++ clang/include/clang/Basic/TargetBuiltins.h
@@ -402,12 +402,22 @@ namespace clang {
     };
   }
 
+  /// MyArch builtins
+  namespace MyArch {
+    enum {
+      LastTIBuiltin = clang::Builtin::FirstTSBuiltin-1,
+#define BUILTIN(ID, TYPE, ATTRS) BI##ID,
+#include "clang/Basic/BuiltinsMyArch.def"
+      LastTSBuiltin
+    };
+  }
+
   static constexpr uint64_t LargestBuiltinID = std::max<uint64_t>(
       {ARM::LastTSBuiltin, AArch64::LastTSBuiltin, BPF::LastTSBuiltin,
        PPC::LastTSBuiltin, NVPTX::LastTSBuiltin, AMDGPU::LastTSBuiltin,
        X86::LastTSBuiltin, VE::LastTSBuiltin, RISCV::LastTSBuiltin,
        Hexagon::LastTSBuiltin, Mips::LastTSBuiltin, XCore::LastTSBuiltin,
-       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin});
+       SystemZ::LastTSBuiltin, WebAssembly::LastTSBuiltin, MyArch::LastTSBuiltin});
 
 } // end namespace clang.
 
--- clang/lib/Basic/Targets/MyArch.cpp
+++ clang/lib/Basic/Targets/MyArch.cpp
@@ -1,15 +1,23 @@
 #include "MyArch.h"
 #include "clang/Basic/Builtins.h"
 #include "clang/Basic/MacroBuilder.h"
+#include "clang/Basic/TargetBuiltins.h"
 
 using namespace clang;
 using namespace clang::targets;
 
+static constexpr Builtin::Info BuiltinInfo[] = {
+#define BUILTIN(ID, TYPE, ATTRS)                                               \
+  {#ID, TYPE, ATTRS, nullptr, HeaderDesc::NO_HEADER, ALL_LANGUAGES},
+#include "clang/Basic/BuiltinsMyArch.def"
+};
+
 void MyArchTargetInfo::getTargetDefines(const LangOptions &Opts,
-                                     MacroBuilder &Builder) const {
+                                        MacroBuilder &Builder) const {
   Builder.defineMacro("__sim__");
 }
 
 ArrayRef<Builtin::Info> MyArchTargetInfo::getTargetBuiltins() const {
-  return std::nullopt;
+  return llvm::ArrayRef(BuiltinInfo,
+                        clang::MyArch::LastTSBuiltin - Builtin::FirstTSBuiltin);
 }
